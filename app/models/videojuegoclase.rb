b0c4702f001b93c5c9b83d6d321d5d900b4f047e
mario <xhi.wike@gmail.com>
2015-11-25 20:56:15 -0300

    cambio datos
    envio un cambio de datos y correccion de codigo

--- app/models/videojuegoclase.rb
+++ app/models/videojuegoclase.rb
@@ -1,6 +1,6 @@
 class Videojuegoclase < ActiveRecord::Base
   belongs_to :videojuego
   belongs_to :clase
-  validates :videojuego, presence: true, length: {in:4..20, too_short:'Tiene que tener al menos 4 caracteres', too_long:'Maxima de 20 caracteres'}
-  validates :clase, presence: true, length: {in:4..15, too_short:'Tiene que tener al menos 4 caracteres', too_long:'Maxima de 15 caracteres'}
+  validates :videojuego, presence: true, length: {in:2..50, too_short:'Tiene que tener al menos 2 caracteres', too_long:'Maxima de 50 caracteres'}
+  validates :clase, presence: true, length: {in:2..30, too_short:'Tiene que tener al menos 2 caracteres', too_long:'Maxima de 30 caracteres'}
 end
